53b99b55aacdc6744cd9ac50c7fb9dd3f38d0ac5
lorfakl <lorfakl@aol.com>
2025-03-17 15:18:31 -0400

    I actually broke the navbar
    I broke the navbar but now I have fixed it
    The fix was actually quite an involved fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import myimage from '../images/myicon.png'
 import { GiHamburgerMenu } from "react-icons/gi";
 import { Link } from 'react-router';
@@ -6,6 +6,27 @@ import { Link } from 'react-router';
 function Navbar()
 {
     const [menuStatus, setMenuStatus] = useState(false)
+    const dropdownRef = useRef(null)
+
+    useEffect(() => {
+        function handleClickOutsideDropdown(event) {
+            console.log("detected a click outside the dropdown element ", event.target)
+            if(dropdownRef && !dropdownRef.current.contains(event.target))
+            {
+                console.log("valid click outside of dropdown")
+                if (menuStatus) {
+                    closeMenu()
+                }
+            }
+        }
+        
+        // Add event listener when menu is open
+        document.addEventListener('mousedown', handleClickOutsideDropdown);
+        
+        // Clean up on unmount
+        return () => { document.removeEventListener('mousedown', handleClickOutsideDropdown) }
+    
+    }, [])
 
     const closeMenu = () => setMenuStatus(false)
     
@@ -20,12 +41,16 @@ function Navbar()
         }
     }
 
+    function handleOffClickEdgeCase(){
+        console.log("Clicked previous panels")
+    }
+
     return(
     <>
         <div className="navbar bg-base-100">
-            <div className="flex-none">
+            <div className="flex-none" ref={dropdownRef}>
                 <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn m-1" onClick={toggleMenu} onBlur={closeMenu}>
+                    <div tabIndex={0} role="button" className="btn m-1" onClick={toggleMenu}>
                         <GiHamburgerMenu size={30}/>
                     </div>
                     {menuStatus ? 
@@ -48,7 +73,6 @@ function Navbar()
                     : 
                     null
                     }
-                    
                 </div>
             </div>
             <div className="flex-1">
